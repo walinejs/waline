1a80450e3fb296e02bf93d5824725d643365c90d
lizheming <i@imnerd.org>
2020-11-08 20:58:10 +0800

    feature: add docker image build

---
+++ packages/server/vanilla.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const Application = require('thinkjs');
+
+const instance = new Application({
+  ROOT_PATH: __dirname,
+  APP_PATH: path.join(__dirname, 'src'),
+  proxy: true, // use proxy
+  env: 'production'
+});
+
+instance.run();
+
+let config = {};
+try {
+  require('./config.js');
+} catch(e) {}
+for(const k in config) {
+  think.config(k, config[k]);
+}
