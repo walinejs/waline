71200b14709b143e114bca06fd34d9d5b39f38c8
lizheming <i@imnerd.org>
2020-11-08 13:24:25 +0800

    fix: config error

--- packages/server/index.js
+++ packages/server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Application = require('thinkjs');
 const Loader = require('thinkjs/lib/loader');
 
-module.exports = function(config) {
+module.exports = function(config = {}) {
   const app = new Application({
     ROOT_PATH: __dirname,
     APP_PATH: path.join(__dirname, 'src'),
@@ -17,7 +17,10 @@ module.exports = function(config) {
   loader.loadAll('worker');
 
   return function (req, res) {
-    think.config(config);
+    for(const k in config) {
+      think.config(k, config[k]);
+    }
+
     return think.beforeStartServer().catch(err => {
       think.logger.error(err);
     }).then(() => {
