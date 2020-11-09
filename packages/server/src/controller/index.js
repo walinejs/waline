a57f56b83cbac7af58719a58958f76004604de08
lizheming <i@imnerd.org>
2020-11-09 12:05:55 +0800

    chore: change default word

--- packages/server/src/controller/index.js
+++ packages/server/src/controller/index.js
@@ -2,6 +2,7 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
   indexAction() {
-    this.body = 'LeanCloud 无服务评论';
+    this.type = 'html';
+    this.body = 'Welcome to use <a href="https://waline.js.org" target="_blank"><b>Waline</b></a>!';
   }
 };
