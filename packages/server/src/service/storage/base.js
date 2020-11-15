015c66d55febb03045507b51bde541fdaf26725c
lizheming <i@imnerd.org>
2020-11-15 12:25:36 +0800

    chore: initial cloud store base file

--- packages/server/src/service/storage/base.js
+++ packages/server/src/service/storage/base.js
@@ -1,3 +1,23 @@
 module.exports = class extends think.Service {
+  async select(where, {desc, limit, offset, field} = {}) {
+    //to be implemented
+  }
 
+  async count(where = {}, options = {}) {
+    //to be implemented
+  }
+
+  async add(data, {
+    access: {read = true, write = true} = {read: true, write: true}
+  } = {}) {
+    //to be implemented
+  }
+
+  async update(data, where) {
+    //to be implemented
+  }
+
+  async delete(where) {
+    //to be implemented
+  }
 }
