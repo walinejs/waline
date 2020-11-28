15329bbde8b9bea5c1b5fc35cba01d0a3533d226
lizheming <i@imnerd.org>
2020-11-28 11:20:24 +0800

    docs: custome waline support

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,24 +5,21 @@ function waline() {
     serverURL: 'https://waline.vercel.app',
     path: window.location.pathname,
     visitor: true,
+    lang: location.pathname.startsWith('/en/') ? 'en' : 'zh-CN' 
   });
 }
 
-function renderValine(router) {
-  router.afterEach(route => {
+function renderWaline(router) {
+  router.afterEach(_ => {
     let $page = document.querySelector('.page')
     let container = document.getElementById('waline-comment')
     if(container) {
       $page.removeChild(container);
     }
     
-    if(route.path !== '/' && route.path !== '/visitor.html') {
-      return;
-    }
-    
     container = document.createElement('div')
-    container.id = route.path === '/' ? 'waline-comment' : 'waline-comment-invisible';
-    container.className = 'page-nav'
+    container.id = 'waline-comment';
+    container.className = 'page-nav';
 
     if ($page){
       $page.appendChild(container)
@@ -32,14 +29,14 @@ function renderValine(router) {
         $page = document.querySelector('.page')
         $page.appendChild(container)
         waline()
-      }, 1000)
+      }, 1500)
     }
   })
 }
 
 export default ({router}) => {
   try {
-    document && renderValine(router)
+    document && renderWaline(router)
   } catch (e) {
     console.error(e.message)
   }
