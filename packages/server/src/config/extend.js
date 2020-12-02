afb294813bd42bb0ad91bddc454cc4dab8b0e552
lizheming <i@imnerd.org>
2020-12-02 21:24:25 +0800

    chore: all fail set 500 status code

--- packages/server/src/config/extend.js
+++ packages/server/src/config/extend.js
@@ -3,5 +3,15 @@ const Mongo = require('think-mongo');
 
 module.exports = [
   Model(think.app),
-  Mongo(think.app)
+  Mongo(think.app),
+  {
+    controller: {
+      fail(...args) {
+        if(this.ctx.status === 200) {
+          this.ctx.status = 500;
+        }
+        this.ctx.fail(...args);
+      }
+    }
+  }
 ];
