32ba611212ffed89b2b31b542729c5228f1c9e6f
lizheming <i@imnerd.org>
2020-12-02 21:35:38 +0800

    chore: add example waline to server

--- packages/server/src/controller/index.js
+++ packages/server/src/controller/index.js
@@ -1,6 +1,24 @@
 module.exports = class extends think.Controller {
   indexAction() {
     this.type = 'html';
-    this.body = 'Welcome to use <a href="https://waline.js.org" target="_blank"><b>Waline</b></a>!';
+    this.body = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Waline Example</title>
+    </head>
+    <body>
+      <div id="waline" style="max-width: 800px;margin: 0 auto;"></div> <script src="https://cdn.jsdelivr.net/npm/@waline/client/dist/Waline.min.js"></script>
+      <script>
+        new Waline({
+          el: '#waline',
+          path: '/',
+          serverURL: location.protocol + '//' + location.host
+        });
+      </script>
+    </body>
+    </html>`;
   }
 };
