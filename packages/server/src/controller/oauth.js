9ac07fea749b42ae2bdbbb6e47efd97e0294f90a
lizheming <i@imnerd.org>
2021-01-09 18:15:11 +0800

    fix(vercel): reconstructor github social login

--- packages/server/src/controller/oauth.js
+++ packages/server/src/controller/oauth.js
@@ -9,6 +9,19 @@ module.exports = class extends think.Controller {
   async githubAction() {
     const instance = this.service('auth/github', this);
     const userInfo = await instance.getUserInfo();
+    const {github} = userInfo;
+
+    const userByGithub = await this.modelInstance.select({github});
+    if(!think.isEmpty(userByGithub)) {
+      const {redirect} = this.get();
+      const token = jwt.sign(email, this.config('jwtKey'));
+      if(redirect) {
+        return this.redirect(redirect + (redirect.includes('?') ? '&' : '?') + 'token=' + token);
+      }
+
+      return this.success();
+    }
+
     if(!userInfo.email) {
       //generator a fake email if github user have no email
       userInfo.email = `${userInfo.github}@mail.github`;
@@ -16,30 +29,29 @@ module.exports = class extends think.Controller {
 
     const {github, email} = userInfo;
     const current = this.ctx.state.userInfo;
-
-    if(think.isEmpty(current)) {
-      const user = await this.modelInstance.select({email});
-      if(think.isEmpty(user)) {
-        const count = await this.modelInstance.count();
-        const data = {
-          ...userInfo,
-          password: (new PasswordHash()).hashPassword(Math.random()),
-          type: think.isEmpty(count) ? 'administrator' : 'guest'
-        };
-        await this.modelInstance.add(data);
-      } else {
-        await this.modelInstance.update({github}, {email});
-      }
-
-      const {redirect} = this.get();
-      const token = jwt.sign(email, this.config('jwtKey'));
-      if(redirect) {
-        return this.redirect(redirect + (redirect.includes('?') ? '&' : '?') + 'token=' + token);
-      }
-    } else {
+    if(!think.isEmpty(current)) {
       await this.modelInstance.update({github}, {objectId: current.objectId});
+      return this.success();
     }
 
+    const userByEmail = await this.modelInstance.select({email});
+    if(think.isEmpty(userByEmail)) {
+      const count = await this.modelInstance.count();
+      const data = {
+        ...userInfo,
+        password: (new PasswordHash()).hashPassword(Math.random()),
+        type: think.isEmpty(count) ? 'administrator' : 'guest'
+      };
+      await this.modelInstance.add(data);
+    } else {
+      await this.modelInstance.update({github}, {email});
+    }
+
+    const {redirect} = this.get();
+    const token = jwt.sign(email, this.config('jwtKey'));
+    if(redirect) {
+      return this.redirect(redirect + (redirect.includes('?') ? '&' : '?') + 'token=' + token);
+    }
     return this.success();
   }
 };
