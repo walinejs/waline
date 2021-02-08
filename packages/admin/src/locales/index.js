17da16efe9aee19bf952da4f6ca314948d42617a
Jerry Wong <hwy0127@gmail.com>
2021-02-08 19:37:14 +0800

    add zh-TW (#149)
    * feat: 增加 MAIL_SUBJECT_ADMIN 和 MAIL_TEMPLATE_ADMIN 配置
    improvement: 修改部分zh-tw 用詞
    
    * add zh-TW

--- packages/admin/src/locales/index.js
+++ packages/admin/src/locales/index.js
@@ -1,12 +1,13 @@
 import en from './en.json';
 import zhCN from './zh-CN.json';
+import zhTW from './zh-TW.json';
 
 export default {
   "zh-cn": { translations: zhCN },
   "zh-CN": { translations: zhCN },
   "en": { translations: en },
   "en-US": { translations: en },
-  "zh-TW": { translations: en },
+  "zh-TW": { translations: zhTW },
   "jp": { translations: en },
   "jp-JP": { translations: en }
 };
