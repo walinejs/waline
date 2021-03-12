613323c5b91640a81936bda5378f54a47abbfdea
lizheming <i@imnerd.org>
2021-03-12 20:46:29 +0800

    fix(vercel): compat pgsql column case insensitive

--- packages/server/src/service/storage/postgresql.js
+++ packages/server/src/service/storage/postgresql.js
@@ -5,6 +5,24 @@ module.exports = class extends MySQL {
     return super.model(tableName.toLowerCase());
   }
 
+  async select(...args) {
+    const data = await super.select(...args);
+    return data.map(({insertedat, createdat, updatedat, ...item}) => {
+      const mapFields = {
+        insertedAt: insertedat,
+        createdAt: createdat,
+        updatedAt: updatedat
+      };
+      for(const field in mapFields) {
+        if(!mapFields[field]) {
+          continue;
+        }
+        item[field] = mapFields[field];
+      }
+      return item;
+    });
+  }
+  
   async count(...args) {
     let result = await super.count(...args);
     try {
