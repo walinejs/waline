62528b90282b456ddf0a480db422f4f9bc3e4693
Mr.Hope <zhangbowang1998@gmail.com>
2021-04-29 16:28:01 +0800

    refactor: bump husky to v6 add add lint-staged

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const fs = require('fs');
+const path = require('path');
+
+const packages = fs.readdirSync(path.resolve(__dirname, 'packages'));
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', ['release', ...packages]],
+  },
+};
