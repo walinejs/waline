1425f9b195008bfd4004adbe13eba1012dcf269a
Mr.Hope <zhangbowang1998@gmail.com>
2021-04-30 13:38:23 +0800

    feat(client): use attribute for math load (#224)

--- packages/client/src/utils/mathml.js
+++ packages/client/src/utils/mathml.js
@@ -14,6 +14,6 @@ export default function () {
   document.body.removeChild(div);
 
   if (Math.abs(box.height - 23) > 1 || Math.abs(box.width - 77) > 1) {
-    document.body.className += ' mathml-polyfill';
+    document.body.setAttribute('waline-math');
   }
 }
