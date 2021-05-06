5be12dff80675d8836ac000ddecdfdf82f696025
Mr.Hope <zhangbowang1998@gmail.com>
2021-05-06 17:58:22 +0800

    fix(client): fix avatarForce not working

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -35,7 +35,7 @@ export function ReactComponent({
       emojiMaps={emojiMaps}
       avatar={avatar}
       avatarCDN={avatarCDN}
-      avatarFore={avatarForce}
+      avatarForce={avatarForce}
       uploadImage={uploadImage}
       wordLimit={wordLimit}
     >
