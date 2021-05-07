520042ec78d4b0ab17ba1f3cabaa1c27e389a05d
Mr.Hope <zhangbowang1998@gmail.com>
2021-05-07 18:25:01 +0800

    feat(client): use border as default

--- packages/client/src/utils/dark.js
+++ packages/client/src/utils/dark.js
@@ -1,4 +1,4 @@
-const style = `{--waline-white: #000;--waline-bgcolor: #1e1e1e;--waline-light:#272727;--waline-light-grey:#666;--waline-dark-grey:#999;--waline-text-color:#888;--waline-border-color: #333;--waline-disable-bgcolor: #222;--waline-disable-color:#272727;--waline-box-shadow: 0 12px 40px #0f0e0d;--waline-bq-color: #272727;--waline-info-bgcolor: #272727;--waline-info-color: #666;}`;
+const style = `{--waline-white: #000;--waline-bgcolor: #1e1e1e;--waline-light:#272727;--waline-light-grey:#666;--waline-dark-grey:#999;--waline-text-color:#888;--waline-border-color: #333;--waline-disable-bgcolor: #222;--waline-disable-color:#272727;--waline-bq-color: #272727;--waline-info-bgcolor: #272727;--waline-info-color: #666;}`;
 
 const injectStyle = (content) => {
   if (!document.body.hasAttribute('waline-dark')) {
