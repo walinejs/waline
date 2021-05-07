61d3528c08375ffb05ea4003de6b040176ef75d2
Mr.Hope <zhangbowang1998@gmail.com>
2021-05-07 15:58:58 +0800

    refactor(client): format style

--- packages/client/src/utils/dark.js
+++ packages/client/src/utils/dark.js
@@ -1,6 +1,6 @@
 const style = `{--waline-white: #000;--waline-light:#1e1e1e;--waline-light-grey:#666;--waline-dark-grey:#999;
 --waline-text-color:#888;--waline-border-color: #333;--waline-disable-bgcolor: #222;--waline-disable-color:#272727;
---waline-box-shadow: 0px 12px 40px #0f0e0d;--waline-bq-color: #272727;--waline-info-bgcolor: #272727;
+--waline-box-shadow: 0 12px 40px #0f0e0d;--waline-bq-color: #272727;--waline-info-bgcolor: #272727;
 --waline-info-color: #666;}`;
 
 const injectStyle = (content) => {
