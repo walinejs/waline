650d9f131f79397ec3311bd5dbfd19925e8966c1
Mr.Hope <zhangbowang1998@gmail.com>
2021-05-19 19:52:06 +0800

    test(client): update client test

--- packages/client/__tests__/emoji.spec.ts
+++ packages/client/__tests__/emoji.spec.ts
@@ -1,21 +1,30 @@
-import { parseEmoji, emojiCDN, emojiMaps } from '../src/utils';
+import { defaultEmojiCDN, defaultEmojiMaps } from '../src/config';
+import { parseEmoji } from '../src/utils';
 
 describe('Emoji test', () => {
   it('Should not parse', () => {
     const content = 'Waline is a good framework. Note: It works with backend';
 
-    expect(parseEmoji(content, emojiMaps, emojiCDN)).toEqual(content);
+    expect(parseEmoji(content, defaultEmojiMaps, defaultEmojiCDN)).toEqual(
+      content
+    );
   });
 
   it("Should not parse emoji it don't know", () => {
     const content = 'Waline is a good framework. :heart:';
 
-    expect(parseEmoji(content, emojiMaps, emojiCDN)).toEqual(content);
+    expect(parseEmoji(content, defaultEmojiMaps, defaultEmojiCDN)).toEqual(
+      content
+    );
   });
 
   it('Should parse emoji', () => {
     expect(
-      parseEmoji('Waline is a good framework. :money:', emojiMaps, emojiCDN)
+      parseEmoji(
+        'Waline is a good framework. :money:',
+        defaultEmojiMaps,
+        defaultEmojiCDN
+      )
     ).toEqual(
       'Waline is a good framework. <img class="vemoji" src="https://img.t.sinajs.cn/t4/appstyle/expression/ext/normal/a2/2018new_qian_thumb.png" alt="money">'
     );
