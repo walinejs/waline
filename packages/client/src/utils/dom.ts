0b405423b0a83e161133dfe8629deb0ade592804
Mr.Hope <zhangbowang1998@gmail.com>
2021-06-01 14:43:44 +0800

    fix(client): fix dark hover color

--- packages/client/src/utils/dom.ts
+++ packages/client/src/utils/dom.ts
@@ -1,4 +1,4 @@
-const style = `{--waline-white:#000;--waline-light-grey:#666;--waline-dark-grey:#999;--waline-text-color:#888;--waline-bgcolor:#1e1e1e;--waline-bgcolor-light:#272727;--waline-border-color:#333;--waline-disable-bgcolor:#444;--waline-disable-color:#272727;--waline-bq-color:#272727;--waline-info-bgcolor:#272727;--waline-info-color:#666}`;
+const style = `{--waline-white:#000;--waline-light-grey:#666;--waline-dark-grey:#999;--waline-text-color:#888;--waline-bgcolor:#1e1e1e;--waline-bgcolor-light:#272727;--waline-bgcolor-hover: #444;--waline-border-color:#333;--waline-disable-bgcolor:#444;--waline-disable-color:#272727;--waline-bq-color:#272727;--waline-info-bgcolor:#272727;--waline-info-color:#666}`;
 
 const injectStyle = (content: string): void => {
   if (!document.body.hasAttribute('waline-dark')) {
