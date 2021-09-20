39de903cc0d18414ee86731f6e09f69bb51b8c24
lizheming <i@imnerd.org>
2021-09-21 00:15:40 +0800

    doc: add api doc

--- packages/server/src/logic/token.js
+++ packages/server/src/logic/token.js
@@ -1,3 +1,46 @@
 const Base = require('./base');
 
-module.exports = class extends Base {};
+module.exports = class extends Base {
+  /**
+   * @api {GET} /token  get login user info
+   * @apiGroup User
+   * @apiVersion  0.0.1
+   *
+   * @apiSuccess  (200) {Number}  errno 0
+   * @apiSuccess  (200) {String}  errmsg  return error message if error
+   * @apiSuccess  (200) {Object}  data user info
+   * @apiSuccess  (200) {String}  data.avatar user avatar
+   * @apiSuccess  (200) {String}  data.createdAt user register time
+   * @apiSuccess  (200) {String}  data.display_name user nick name
+   * @apiSuccess  (200) {String}  data.emal user email address
+   * @apiSuccess  (200) {String}  data.github user github account name
+   * @apiSuccess  (200) {String}  data.mailMd5 user mail md5
+   * @apiSuccess  (200) {String}  data.objectId user id
+   * @apiSuccess  (200) {String}  data.type user type, administrator or guest
+   * @apiSuccess  (200) {String}  data.url user link
+   */
+  getAction() {}
+
+  /**
+   * @api {POST} /token user login
+   * @apiGroup User
+   * @apiVersion  0.0.1
+   *
+   * @apiParam  {String}  email login user email
+   * @apiParam  {String}  password login user password
+   *
+   * @apiSuccess  (200) {Number}  errno 0
+   * @apiSuccess  (200) {String}  errmsg  return error message if error
+   */
+  postAction() {}
+
+  /**
+   * @api {DELETE} /token  user logout
+   * @apiGroup User
+   * @apiVersion  0.0.1
+   *
+   * @apiSuccess  (200) {Number}  errno 0
+   * @apiSuccess  (200) {String}  errmsg  return error message if error
+   */
+  deleteAction() {}
+};
--- packages/server/src/logic/user.js
+++ packages/server/src/logic/user.js
@@ -1,6 +1,34 @@
 const Base = require('./base');
 
 module.exports = class extends Base {
+  /**
+   * @api {POST} /user user register
+   * @apiGroup User
+   * @apiVersion  0.0.1
+   *
+   * @apiParam  {String}  display_name  user nick name
+   * @apiParam  {String}  email user email
+   * @apiParam  {String}  password user password
+   * @apiParam  {String}  url user link
+   *
+   * @apiSuccess  (200) {Number}  errno 0
+   * @apiSuccess  (200) {String}  errmsg  return error message if error
+   */
+  postAction() {}
+
+  /**
+   * @api {PUT} /user update user profile
+   * @apiGroup User
+   * @apiVersion  0.0.1
+   *
+   * @apiParam  {String}  display_name  user new nick name
+   * @apiParam  {String}  url user new link
+   * @apiParam  {String}  password user new password
+   * @apiParam  {String}  github user github account name
+   *
+   * @apiSuccess  (200) {Number}  errno 0
+   * @apiSuccess  (200) {String}  errmsg  return error message if error
+   */
   putAction() {
     const { userInfo } = this.ctx.state;
     if (think.isEmpty(userInfo)) {
