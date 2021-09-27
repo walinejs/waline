fba0abdc5951869debcee209b97fba4705b3b1fb
dannyloxavier <dannylo.xavier@gmail.com>
2021-09-27 11:59:08 -0300

    Create new translation for pt-BR
    Portuguese-Brazilian

---
+++ packages/client/src/config/i18n/pt-BR.ts
@@ -0,0 +1,34 @@
+import type { Locale } from './typings';
+
+const ptBRLocale: Locale = {
+  nick: 'Apelido',
+  nickError: 'Apelido não pode ser menor que 3 bytes.',
+  mail: 'E-Mail',
+  mailError: 'Por favor, confirme seu endereço de e-mail.',
+  link: 'Website',
+  placeholder: 'Comente aqui...',
+  sofa: 'Nenhum comentário, ainda.',
+  submit: 'Enviar',
+  reply: 'Responder',
+  cancelReply: 'Cancelar resposta',
+  comment: 'Comentários',
+  more: 'Carregar Mais...',
+  preview: 'Visualizar',
+  emoji: 'Emoji',
+  uploadImage: 'Enviar Imagem',
+  seconds: 'segundos atrás',
+  minutes: 'minutos atrás',
+  hours: 'horas atrás',
+  days: 'dias atrás',
+  now: 'agora mesmo',
+  uploading: 'Enviando',
+  login: 'Entrar',
+  logout: 'Sair',
+  admin: 'Admin',
+  sticky: 'Sticky',
+  word: 'Palavras',
+  wordHint:
+    'Favor enviar comentário com $0 a $1 palavras!\n Número de palavras atuais: $2',
+};
+
+export default ptBRLocale;
