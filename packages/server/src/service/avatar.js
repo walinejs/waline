aeb0df8e52ee4d3f82b4e6f5b4201843b98a5d39
lizheming <i@imnerd.org>
2022-03-26 11:13:54 +0800

    chore: add qq default avatar parser

--- packages/server/src/service/avatar.js
+++ packages/server/src/service/avatar.js
@@ -5,6 +5,15 @@ const { GRAVATAR_STR } = process.env;
 const env = new nunjucks.Environment();
 env.addFilter('md5', (str) => helper.md5(str));
 
+const DEFAULT_GRAVATAR_STR = `{%- set numExp = r/^[0-9]+$/g -%}
+{%- set qqMailExp = r/^[0-9]+@qq.com$/ig -%}
+{%- if numExp.test(nick) -%}
+  https://q1.qlogo.cn/g?b=qq&nk={{nick}}&s=100
+{%- elif qqMailExp.test(mail) -%}
+  https://q1.qlogo.cn/g?b=qq&nk={{mail|replace('@qq.com', '')}}&s=100
+{%- else -%}
+  https://seccdn.libravatar.org/avatar/{{mail|md5}}
+{%- endif -%}`;
 module.exports = class extends think.Service {
   async stringify(comment) {
     const fn = think.config('avatarUrl');
@@ -15,8 +24,7 @@ module.exports = class extends think.Service {
       }
     }
 
-    const gravatarStr =
-      GRAVATAR_STR || 'https://seccdn.libravatar.org/avatar/{{mail|md5}}';
+    const gravatarStr = GRAVATAR_STR || DEFAULT_GRAVATAR_STR;
     return env.renderString(gravatarStr, comment);
   }
 };
