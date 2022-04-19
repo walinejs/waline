6a4630b794efce2d7f60847cef021294ea7eb3e6
Austin Lee <i@imnerd.org>
2022-04-19 20:18:23 +0800

    revert: revert vi language

--- packages/client/src/config/i18n/index.ts
+++ packages/client/src/config/i18n/index.ts
@@ -4,7 +4,6 @@ import zhCN from './zh-CN';
 import zhTW from './zh-TW';
 import ptBR from './pt-BR';
 import ru from './ru';
-import vi from './vi-VN';
 
 import type { Locale } from './typings';
 
@@ -28,7 +27,4 @@ export const locales: Locales = {
   ru: ru,
   'ru-ru': ru,
   'ru-RU': ru,
-  vi: vi,
-  'vi-vn': vi,
-  'vi-VN': vi,
 };
