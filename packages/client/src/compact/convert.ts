17d2b5a2762286b35fef5b80ff8bf2a99303db78
Mr.Hope <mister-hope@outlook.com>
2022-04-24 19:57:10 +0800

    chore(client): tweaks

--- packages/client/src/compact/convert.ts
+++ packages/client/src/compact/convert.ts
@@ -51,7 +51,7 @@ export const covertOptions = (
     warning(`Option "${item}" is REMOVED and CAN NOT be polyfilled!`)
   );
 
-  // warnings with those which can no longr be handled
+  // warnings with those which is being polyfilled
   DROPPED_OPTIONS_WHICH_CAN_STILL_BE_POLYFILLED.filter(([oldOption]) =>
     Object.keys(options).includes(oldOption)
   ).forEach(([oldOption, newOption]) =>
