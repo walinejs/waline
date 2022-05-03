c08657d89bc2f39f2a553c56b411d938555e4d46
lizheming <i@imnerd.org>
2022-05-03 16:27:20 +0800

    feat: add level custom

--- packages/server/src/controller/comment.js
+++ packages/server/src/controller/comment.js
@@ -305,36 +305,43 @@ module.exports = class extends BaseRest {
           );
         }
 
-        const countWhere = {
-          status: ['NOT IN', ['waiting', 'spam']],
-          _complex: {},
-        };
-        if (user_ids.length) {
-          countWhere._complex.user_id = ['IN', user_ids];
-        }
-        const mails = Array.from(
-          new Set(comments.map(({ mail }) => mail).filter((v) => v))
-        );
-        if (mails.length) {
-          countWhere._complex.mail = ['IN', mails];
-        }
-        if (!think.isEmpty(countWhere._complex)) {
-          countWhere._complex._logic = 'or';
-        } else {
-          delete countWhere._complex;
-        }
-        const counts = await this.modelInstance.count(countWhere, {
-          group: ['user_id', 'mail'],
-        });
-        comments.forEach((cmt) => {
-          const countItem = (counts || []).find(({ mail, user_id }) => {
-            if (user_id) {
-              return user_id === cmt.user_id;
-            }
-            return mail === cmt.mail;
+        if (think.isArray(this.config('levels'))) {
+          const countWhere = {
+            status: ['NOT IN', ['waiting', 'spam']],
+            _complex: {},
+          };
+          if (user_ids.length) {
+            countWhere._complex.user_id = ['IN', user_ids];
+          }
+          const mails = Array.from(
+            new Set(comments.map(({ mail }) => mail).filter((v) => v))
+          );
+          if (mails.length) {
+            countWhere._complex.mail = ['IN', mails];
+          }
+          if (!think.isEmpty(countWhere._complex)) {
+            countWhere._complex._logic = 'or';
+          } else {
+            delete countWhere._complex;
+          }
+          const counts = await this.modelInstance.count(countWhere, {
+            group: ['user_id', 'mail'],
           });
-          cmt.count = countItem ? countItem.count : 0;
-        });
+          comments.forEach((cmt) => {
+            const countItem = (counts || []).find(({ mail, user_id }) => {
+              if (user_id) {
+                return user_id === cmt.user_id;
+              }
+              return mail === cmt.mail;
+            });
+
+            cmt.level = countItem
+              ? this.config('levels').findLast(
+                  (level) => level <= countItem.count
+                )
+              : 0;
+          });
+        }
 
         return this.json({
           page,
