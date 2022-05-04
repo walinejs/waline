0644923bcbef24a507d523bd107b79232c0a1433
lizheming <i@imnerd.org>
2022-05-04 11:27:57 +0800

    feat: add region show for admin

--- packages/admin/src/utils/request.js
+++ packages/admin/src/utils/request.js
@@ -23,7 +23,7 @@ export default async function request(url, opts = {}) {
     opts.headers.Authorization = `Bearer ${token}`;
   }
 
-  let baseUrl = window.serverURL;
+  let baseUrl = 'http://localhost:9090/'; //window.serverURL;
   if (!baseUrl) {
     const match = location.pathname.match(/(.*?\/)ui/);
     baseUrl = match ? match[1] : '/';
