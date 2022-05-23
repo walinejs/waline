b407efadec60e579327233807b73329719679be3
Mr.Hope <mister-hope@outlook.com>
2022-05-24 00:40:31 +0800

    docs: fix docs

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -98,5 +98,13 @@ export default defineUserConfig({
     }),
   ],
 
+  extendsBundlerOptions: (config, app) => {
+    config.viteOptions.ssr = config.viteOptions.ssr ?? {};
+
+    config.viteOptions.ssr.noExternal = config.viteOptions.ssr.noExternal ?? [];
+
+    config.viteOptions.ssr.noExternal.push('@mr-hope/vuepress-shared');
+  },
+
   shouldPrefetch: false,
 });
