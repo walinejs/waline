bbc06fa3aa5ad4572b599b3f33413bae61b820b8
lizheming <i@imnerd.org>
2022-09-25 19:08:57 +0800

    feat: add reaction get and set for server

--- packages/server/src/logic/article.js
+++ packages/server/src/logic/article.js
@@ -4,6 +4,24 @@ module.exports = class extends Base {
   getAction() {
     this.rules = {
       path: { array: true },
+      type: { array: true, default: ['time'] },
+    };
+  }
+
+  postAction() {
+    this.rules = {
+      path: {
+        string: true,
+      },
+      type: {
+        string: true,
+        default: 'time',
+      },
+      action: {
+        string: true,
+        in: ['inc', 'desc'],
+        default: 'inc',
+      }
     };
   }
 };
