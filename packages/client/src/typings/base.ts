4978ebb4785a57fb5c659b6d62ebed70415050aa
Mr.Hope <mister-hope@outlook.com>
2022-11-26 16:12:36 +0800

    chore(client): fix types

--- packages/client/src/typings/base.ts
+++ packages/client/src/typings/base.ts
@@ -1,6 +1,9 @@
 export type WalineCommentSorting = 'latest' | 'oldest' | 'hottest';
 
-export type WalineEmojiPresets = `http://${string}` | `https://${string}`;
+export type WalineEmojiPresets =
+  | `//${string}`
+  | `http://${string}`
+  | `https://${string}`;
 
 export interface WalineEmojiInfo {
   /**
