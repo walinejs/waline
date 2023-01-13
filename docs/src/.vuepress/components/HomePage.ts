988c36dabbdcf09bce30bc7cbe31bd6e62580823
Mr.Hope <mister-hope@outlook.com>
2023-01-13 23:14:11 +0800

    docs: fix docs

--- docs/src/.vuepress/components/HomePage.ts
+++ docs/src/.vuepress/components/HomePage.ts
@@ -2,7 +2,7 @@ import { defineComponent, h, resolveComponent } from 'vue';
 import HopeHomePage from 'vuepress-theme-hope/components/HomePage';
 import WalineTips from './WalineTips.js';
 
-import { useDarkMode } from '@theme-hope/modules/outlook/composables/index';
+import { useDarkmode } from '@theme-hope/modules/outlook/composables/index';
 
 import type { VNode } from 'vue';
 
@@ -10,7 +10,7 @@ export default defineComponent({
   name: 'HopePage',
 
   setup() {
-    const { isDarkMode } = useDarkMode();
+    const { isDarkMode } = useDarkmode();
 
     return (): VNode =>
       h(
--- docs/src/.vuepress/components/NormalPage.ts
+++ docs/src/.vuepress/components/NormalPage.ts
@@ -1,5 +1,6 @@
 import { usePageFrontmatter } from '@vuepress/client';
 import { computed, defineComponent, h, resolveComponent } from 'vue';
+import { RenderDefault, hasGlobalComponent } from 'vuepress-shared/client';
 
 import BreadCrumb from '@theme-hope/components/BreadCrumb';
 import MarkdownContent from '@theme-hope/components/MarkdownContent';
@@ -12,9 +13,9 @@ import { useThemeLocaleData } from '@theme-hope/composables/index';
 import PageMeta from '@theme-hope/modules/info/components/PageMeta';
 import TOC from '@theme-hope/modules/info/components/TOC';
 
-import { useDarkMode } from '@theme-hope/modules/outlook/composables/index';
+import { useDarkmode } from '@theme-hope/modules/outlook/composables/index';
 
-import type { VNode } from 'vue';
+import type { DefineComponent, VNode } from 'vue';
 import type { ThemeNormalPageFrontmatter } from 'vuepress-theme-hope';
 
 import 'vuepress-theme-hope/styles/page.scss';
@@ -24,7 +25,7 @@ export default defineComponent({
 
   setup(_props, { slots }) {
     const frontmatter = usePageFrontmatter<ThemeNormalPageFrontmatter>();
-    const { isDarkMode } = useDarkMode();
+    const { isDarkMode } = useDarkmode();
     const themeLocale = useThemeLocaleData();
 
     const tocEnable = computed(
@@ -34,26 +35,39 @@ export default defineComponent({
     );
 
     return (): VNode =>
-      h('main', { class: 'page', id: 'main-content' }, [
-        slots.top?.(),
-        h(BreadCrumb),
-        h(PageTitle),
-        tocEnable.value
-          ? h(TOC, {
-              headerDepth:
-                frontmatter.value.headerDepth ?? themeLocale.value.headerDepth,
-            })
-          : null,
-        slots.contentBefore?.(),
-        h(MarkdownContent),
-        slots.contentAfter?.(),
-        h(PageMeta),
-        h(PageNav),
-        h(WalineTips),
-        h(resolveComponent('CommentService'), {
-          darkmode: isDarkMode.value,
-        }),
-        slots.bottom?.(),
-      ]);
+      h(
+        'main',
+        { class: 'page', id: 'main-content' },
+        h(
+          hasGlobalComponent('LocalEncrypt')
+            ? <DefineComponent>resolveComponent('LocalEncrypt')
+            : RenderDefault,
+          () => [
+            slots['top']?.(),
+            h(BreadCrumb),
+            h(PageTitle),
+            tocEnable.value
+              ? h(TOC, {
+                  headerDepth:
+                    frontmatter.value.headerDepth ??
+                    themeLocale.value.headerDepth ??
+                    2,
+                })
+              : null,
+            slots['contentBefore']?.(),
+            h(MarkdownContent),
+            slots['contentAfter']?.(),
+            h(PageMeta),
+            h(PageNav),
+            h(WalineTips),
+            hasGlobalComponent('CommentService')
+              ? h(resolveComponent('CommentService'), {
+                  darkmode: isDarkMode.value,
+                })
+              : null,
+            slots['bottom']?.(),
+          ]
+        )
+      );
   },
 });
