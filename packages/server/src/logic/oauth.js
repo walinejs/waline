942321fe3516d9b8d59527366e58b8f6e4065dac
Austin Lee <i@imnerd.org>
2023-04-01 10:28:00 +0800

    [FEAT] @waline/vercel@2 (#1904)
    * feat: add `/api` prefix
    
    * fix: return time timestamp to fix #1539
    
    * feat: /api/comment?type=count & /api/article return inconstant data type number[]
    
    /api/comment & /api/article return data with {errno, errmsg, data} struct
    
    * chore: update apidoc with `/api` prefix
    
    * chore: admin page compat with `/api` prefix api
    
    * fix: fix #1649
    
    * doc: update api doc
    
    ---------
    
    Co-authored-by: Mr.Hope <mister-hope@outlook.com>

--- packages/server/src/logic/oauth.js
+++ packages/server/src/logic/oauth.js
@@ -2,7 +2,7 @@ const Base = require('./base');
 
 module.exports = class extends Base {
   /**
-   * @api {GET} /oauth oauth api
+   * @api {GET} /api/oauth oauth api
    * @apiGroup  OAuth
    * @apiVersion  0.0.1
    */
