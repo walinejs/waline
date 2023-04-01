cd73adce7d71d075a256860e6415fc418a41bb22
lizheming <i@imnerd.org>
2023-04-02 01:00:11 +0800

    chore: add netlify support

--- packages/server/src/config/middleware.js
+++ packages/server/src/config/middleware.js
@@ -6,6 +6,11 @@ const isTcb = think.env === 'cloudbase';
 const isDeta = think.env === 'deta' || process.env.DETA_RUNTIME === 'true';
 const isAliyunFC =
   think.env === 'aliyun-fc' || Boolean(process.env.FC_RUNTIME_VERSION);
+const isNetlify = process.env.NETLIFY_IMAGES_CDN_DOMAIN && process.env._HANDLER;
+const netlifyFunctionPrefix = `/.netlify/functions/${process.env._HANDLER.replace(
+  /\.handler$/,
+  ''
+)}`;
 
 module.exports = [
   {
@@ -16,13 +21,13 @@ module.exports = [
   {
     handle: 'prefix-warning',
   },
-  
   {
     handle: 'meta',
     options: {
       logRequest: isDev,
       sendResponseTime: isDev,
-      requestTimeoutCallback: isTcb || isDeta || isAliyunFC ? false : () => {},
+      requestTimeoutCallback:
+        isTcb || isDeta || isAliyunFC || isNetlify ? false : () => {},
     },
   },
 
@@ -62,7 +67,7 @@ module.exports = [
   {
     handle: 'router',
     options: {
-      prefix: ['/api']
+      prefix: ['/api', netlifyFunctionPrefix, `${netlifyFunctionPrefix}/api`],
     },
   },
 
