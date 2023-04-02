c13e37753f26c5dd4a23e52e8eb42f6306ecd700
lizheming <i@imnerd.org>
2023-04-02 12:20:47 +0800

    fix: _HANDLER may not exist

--- packages/server/src/config/netlify.js
+++ packages/server/src/config/netlify.js
@@ -1,6 +1,6 @@
 exports.isNetlify =
   process.env.NETLIFY_IMAGES_CDN_DOMAIN && process.env._HANDLER;
-exports.netlifyFunctionPrefix = `/.netlify/functions/${process.env._HANDLER.replace(
+exports.netlifyFunctionPrefix = `/.netlify/functions/${process.env?._HANDLER?.replace(
   /\.handler$/,
   ''
 )}`;
