40f24a68c173a3564f2be13efeaf843a1af5e640
Mr.Hope <mister-hope@outlook.com>
2023-05-09 12:11:43 +0800

    fix(client): add missing export

--- packages/client/src/composables/index.ts
+++ packages/client/src/composables/index.ts
@@ -2,4 +2,5 @@ export * from './inputs.js';
 export * from './like.js';
 export * from './reaction.js';
 export * from './recaptchaV3.js';
+export * from './turnstile.js';
 export * from './userInfo.js';
