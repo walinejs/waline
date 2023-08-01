93f3653a2923cdb7f76f842216ad91e5b0804cc3
diyanqi <diyanqi07@gmail.com>
2023-08-01 12:16:57 +0800

    无需报错

--- packages/server/src/controller/comment.js
+++ packages/server/src/controller/comment.js
@@ -293,9 +293,7 @@ module.exports = class extends BaseRest {
       data.like =
         (Number(oldData.like) || 0) +
         (data.like ? Math.ceil(Math.random() * likeIncMax) : -1);
-      if (data.like < 0) {
-        return this.fail(this.locale('Like less than zero'));
-      }
+      data.like = Math.max(data.like, 0);
     }
 
     const preUpdateResp = await this.hook('preUpdate', {
