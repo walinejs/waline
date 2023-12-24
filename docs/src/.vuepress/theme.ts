a5e55c1a6f845feebd42abd2658dc0888519ba29
Austin Lee <i@imnerd.org>
2023-12-24 21:38:54 +0800

    doc: enable force login for waline doc

--- docs/src/.vuepress/theme.ts
+++ docs/src/.vuepress/theme.ts
@@ -42,6 +42,7 @@ export default hopeTheme(
 
       comment: {
         provider: 'Waline',
+        login: 'force',
         serverURL: 'https://walinejs.comment.lithub.cc',
         recaptchaV3Key: '6Lfz4-shAAAAANgsYRR0datkzv6zLIaKrSqfHsiG',
         pageview: true,
