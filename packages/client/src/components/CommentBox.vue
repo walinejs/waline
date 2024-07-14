e5b4a886c6ca512cbf9d07da4b6aeb5d06d6ba5f
CWorld <cworld0@qq.com>
2024-07-14 21:41:33 +0800

    feat(client): improve a11y for actions (#2609)
    Co-authored-by: Mister-Hope <mister-hope@outlook.com>

--- packages/client/src/components/CommentBox.vue
+++ packages/client/src/components/CommentBox.vue
@@ -643,6 +643,7 @@ onMounted(() => {
             id="wl-image-upload"
             ref="imageUploadRef"
             class="upload"
+            aria-hidden="true"
             type="file"
             accept=".png,.jpg,.jpeg,.webp,.bmp,.gif"
             @change="onChange"
@@ -653,6 +654,7 @@ onMounted(() => {
             for="wl-image-upload"
             class="wl-action"
             :title="locale.uploadImage"
+            :aria-label="locale.uploadImage"
           >
             <ImageIcon />
           </label>
