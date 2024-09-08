d60df13c9d87cd67f0cbf8aa4388a9baef08b494
Mr.Hope <mister-hope@outlook.com>
2024-09-08 21:41:46 +0800

    chore: bump deps

--- packages/admin/src/pages/profile/twoFactorAuth.jsx
+++ packages/admin/src/pages/profile/twoFactorAuth.jsx
@@ -1,4 +1,4 @@
-import QRCode from 'qrcode.react';
+import { QRCodeSVG } from 'qrcode.react';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
@@ -60,7 +60,7 @@ export default function () {
       {user['2fa'] ? (
         <div>
           <p>{t('enable 2fa')}</p>
-          <QRCode value={data.otpauth_url} size={256} />
+          <QRCodeSVG value={data.otpauth_url} size={256} />
           <br />
           <br />
           <button
@@ -120,7 +120,7 @@ export default function () {
       {!user['2fa'] && step === 3 && (
         <div>
           <p>{t('open app and scan qrcode')}</p>
-          <QRCode value={data.otpauth_url} size={256} />
+          <QRCodeSVG value={data.otpauth_url} size={256} />
           <form method="post" onSubmit={on2faUpdate}>
             <ul className="typecho-option">
               <li>
