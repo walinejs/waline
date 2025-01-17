088b48c3b40beffe597a2fb79f398268c5b92073
Mister-Hope <mister-hope@outlook.com>
2025-01-17 14:08:42 +0800

    chore: tweaks

--- packages/client/src/components/CommentCard.vue
+++ packages/client/src/components/CommentCard.vue
@@ -15,30 +15,24 @@ import { useLikeStorage, useUserInfo } from '../composables/index.js';
 import { getTimeAgo, isLinkHttp } from '../utils/index.js';
 import { configKey } from '../config/index.js';
 
-const props = withDefaults(
-  defineProps<{
-    /**
-     * Comment data
-     */
-    comment: WalineComment;
-    /**
-     * Current comment to be edited
-     */
-    edit?: WalineComment | null;
-    /**
-     * Root comment id
-     */
-    rootId: number;
-    /**
-     * Current comment to be replied
-     */
-    reply?: WalineComment | null;
-  }>(),
-  {
-    edit: null,
-    reply: null,
-  },
-);
+const props = defineProps<{
+  /**
+   * Comment data
+   */
+  comment: WalineComment;
+  /**
+   * Current comment to be edited
+   */
+  edit?: WalineComment | null;
+  /**
+   * Root comment id
+   */
+  rootId: number;
+  /**
+   * Current comment to be replied
+   */
+  reply?: WalineComment | null;
+}>();
 
 const emit = defineEmits<{
   (event: 'log'): void;
