6e4e26a3ee7c61e8f6193e92793de76873f9b20d
martapanc <marta.panc@gmail.com>
2025-11-12 16:30:12 +0100

    feat: add support for Italian language 🇮🇹 (#3291)
    * feat: add support for Italian language
    
    * feat: add support for Italian language - server/src/locales
    
    * chore: add italian lang ito server
    
    ---------
    
    Co-authored-by: Austin Lee <i@imnerd.org>

--- packages/server/src/locales/index.js
+++ packages/server/src/locales/index.js
@@ -1,4 +1,5 @@
 const en = require('./en.json');
+const it = require('./it.json');
 const zhCN = require('./zh-CN.json');
 const zhTW = require('./zh-TW.json');
 
@@ -9,4 +10,6 @@ module.exports = {
   'en-us': en,
   jp: en,
   'jp-jp': en,
+  it,
+  'it-it': it,
 };
