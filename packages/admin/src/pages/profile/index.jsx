e12f3af7bfcb37c73202a4d7f3a194a1bd2d9ece
Austin Lee <i@imnerd.org>
2025-12-20 19:02:02 +0800

    chore: admin panel get oauth service from oauth endpoint
    * chore: fetch oauth info from oauth endpoint
    
    * chore: add oidc field for oidc oauth service
    
    * chore: get oauth service from endpoint adapter

--- packages/admin/src/pages/profile/index.jsx
+++ packages/admin/src/pages/profile/index.jsx
@@ -88,6 +88,10 @@ export default function () {
     token = localStorage.getItem('TOKEN');
   }
 
+  const socials = Array.isArray(window.oauthServices)
+    ? window.oauthServices.map(({ name }) => name)
+    : ['oidc', 'qq', 'weibo', 'github', 'twitter', 'facebook'];
+
   return (
     <>
       <Header />
@@ -197,73 +201,45 @@ export default function () {
               <section id="social-account">
                 <h3>{t('connect to social account')}</h3>
                 <div className="account-list">
-                  {/** warning: compat with old server version */}
-                  {window.ALLOW_SOCIALS && (
+                  {socials.map((social) => (
                     <div
-                      className={cls('account-item github', {
-                        bind: user.github,
+                      key={social}
+                      className={cls('account-item', social, {
+                        bind: user[social],
                       })}
                     >
                       <a
                         href={
-                          user.github
-                            ? `https://github.com/${user.github}`
-                            : `${baseUrl}oauth/github?state=${token}`
+                          user[social]
+                            ? social === 'oidc'
+                              ? 'javascript:void(0)'
+                              : `https://${social}.com/${user[social]}`
+                            : `${baseUrl}oauth?type=${social}&state=${token}`
                         }
-                        target={user.github ? '_blank' : '_self'}
+                        target={user[social] ? '_blank' : '_self'}
                         rel="noreferrer"
                       >
-                        {React.createElement(Icons.github)}
+                        {
+                          /* eslint-disable-next-line import-x/namespace */
+                          React.createElement(Icons[social])
+                        }
                       </a>
-                    </div>
-                  )}
-                  {!window.ALLOW_SOCIALS &&
-                    [
-                      'oidc',
-                      'qq',
-                      'weibo',
-                      'github',
-                      'twitter',
-                      'facebook',
-                    ].map((social) => (
                       <div
-                        key={social}
-                        className={cls('account-item', social, {
-                          bind: user[social],
-                        })}
+                        className="account-unbind"
+                        onClick={() => unbind(social)}
                       >
-                        <a
-                          href={
-                            user[social]
-                              ? social === 'oidc'
-                                ? 'javascript:void(0)'
-                                : `https://${social}.com/${user[social]}`
-                              : `${baseUrl}oauth?type=${social}&state=${token}`
-                          }
-                          target={user[social] ? '_blank' : '_self'}
-                          rel="noreferrer"
+                        <svg
+                          className="close-icon"
+                          viewBox="0 0 1024 1024"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="14"
+                          height="14"
                         >
-                          {
-                            /* eslint-disable-next-line import-x/namespace */
-                            React.createElement(Icons[social])
-                          }
-                        </a>
-                        <div
-                          className="account-unbind"
-                          onClick={() => unbind(social)}
-                        >
-                          <svg
-                            className="close-icon"
-                            viewBox="0 0 1024 1024"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="14"
-                            height="14"
-                          >
-                            <path d="m568.569 512 170.267-170.267c15.556-15.556 15.556-41.012 0-56.569s-41.012-15.556-56.569 0L512 455.431 341.733 285.165c-15.556-15.556-41.012-15.556-56.569 0s-15.556 41.012 0 56.569L455.431 512 285.165 682.267c-15.556 15.556-15.556 41.012 0 56.569 15.556 15.556 41.012 15.556 56.569 0L512 568.569l170.267 170.267c15.556 15.556 41.012 15.556 56.569 0 15.556-15.556 15.556-41.012 0-56.569L568.569 512z" />
-                          </svg>
-                        </div>
+                          <path d="m568.569 512 170.267-170.267c15.556-15.556 15.556-41.012 0-56.569s-41.012-15.556-56.569 0L512 455.431 341.733 285.165c-15.556-15.556-41.012-15.556-56.569 0s-15.556 41.012 0 56.569L455.431 512 285.165 682.267c-15.556 15.556-15.556 41.012 0 56.569 15.556 15.556 41.012 15.556 56.569 0L512 568.569l170.267 170.267c15.556 15.556 41.012 15.556 56.569 0 15.556-15.556 15.556-41.012 0-56.569L568.569 512z" />
+                        </svg>
                       </div>
-                    ))}
+                    </div>
+                  ))}
                 </div>
               </section>
               <br />
