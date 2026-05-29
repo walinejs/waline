5ee4d12088c7bc022eea016a2678f7635a011f98
Mr.Hope <mister-hope@outlook.com>
2026-05-29 15:45:34 +0800

    build: bump deps

--- .ncurc.js
+++ .ncurc.js
@@ -10,12 +10,15 @@ export default {
     return true;
   },
   target: (name) => {
+    // FIXME: now we should be able to upgrade jsdom to v20
+    // as our node engine supports require(esm) in cjs
+    // jsdom v20+ are ESM only
+    if (name === 'jsdom') return 'minor';
+
     if (name.startsWith('@vuepress/') || name === 'vuepress') {
       return '@next';
     }
 
-    // jsdom v20+ are ESM only
-    if (name === 'jsdom') return 'minor';
     if (name === '@types/node') return 'minor';
 
     return 'latest';
