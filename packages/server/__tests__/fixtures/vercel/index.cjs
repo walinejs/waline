28d63cab73d98997748872e3d9790a0fcb188167
lizheming <i@imnerd.org>
2026-06-20 18:32:53 +0800

    chore: try to import mathjax font lib in root

--- packages/server/__tests__/fixtures/vercel/index.cjs
+++ packages/server/__tests__/fixtures/vercel/index.cjs
@@ -1,6 +1,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
+const _mathJaxFont = require('@mathjax/mathjax-newcm-font');
+
 const createApplication = require('../../..');
 
 const storePath =
