9900fc2cb8bd41c418780e3267cf889bc16b870a
lizheming <i@imnerd.org>
2026-06-21 15:09:07 +0800

    fix: fix it

--- packages/server/src/service/markdown/index.js
+++ packages/server/src/service/markdown/index.js
@@ -1,60 +1,65 @@
+const path = require('node:path');
+
 const { katex: katexPlugin } = require('@mdit/plugin-katex');
 const { sub: subPlugin } = require('@mdit/plugin-sub');
 const { createMathjaxInstance, mathjax: mathjaxPlugin } = require('@mdit/plugin-mathjax/sync');
 const { sup: supPlugin } = require('@mdit/plugin-sup');
+
 const MarkdownIt = require('markdown-it');
 const emojiPlugin = require('markdown-it-emoji');
 
 const { resolveHighlighter } = require('./highlight.js');
 const { sanitize } = require('./xss.js');
 
+const mathjaxSvgEntry = require.resolve('@mathjax/mathjax-newcm-font/js/svg.js');
+
+const mathjaxSvgDynamicPath = path.join(path.dirname(mathjaxSvgEntry), 'svg', 'dynamic');
+
 const getMarkdownParser = (markdown = {}) => {
   const { config = {}, plugin = {} } = markdown;
 
-  // markdown-it instance
   const markdownIt = new MarkdownIt({
     breaks: true,
-    linkify: true, // Auto convert URL-like text to links
-    typographer: true, // Enable some language-neutral replacement + quotes beautification
-
-    // default highlight
+    linkify: true,
+    typographer: true,
     highlight: (code, lang) => {
       const highlighter = resolveHighlighter(lang);
 
       return highlighter ? highlighter(code) : '';
     },
-
     ...config,
-
-    // should always enable html option due to parsed emoji
     html: true,
   });
 
   const { emoji, tex, mathjax, katex, sub, sup } = plugin;
 
-  // parse emoji
   if (emoji !== false) {
     markdownIt.use(emojiPlugin.full, typeof emoji === 'object' ? emoji : {});
   }
 
-  // parse sub
   if (sub !== false) {
     markdownIt.use(subPlugin);
   }
 
-  // parse sup
   if (sup !== false) {
     markdownIt.use(supPlugin);
   }
 
-  // parse tex
   if (tex === 'katex') {
     markdownIt.use(katexPlugin, {
       ...katex,
       output: 'mathml',
     });
   } else if (tex !== false) {
-    const mathjaxInstance = createMathjaxInstance({ output: 'svg', mathjax });
+    const mathjaxInstance = createMathjaxInstance({
+      output: 'svg',
+      mathjax,
+      svg: {
+        dynamicPrefix: mathjaxSvgDynamicPath,
+        ...mathjax?.svg,
+      },
+    });
+
     markdownIt.use(mathjaxPlugin, mathjaxInstance);
   }
 
