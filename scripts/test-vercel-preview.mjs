705549a2c4d011a7b0df74e034f2092553cd5baa
Austin Lee <i@imnerd.org>
2026-06-21 15:52:56 +0800

    Revert "chore: revert #3623" (#3660)
    * Revert "chore: revert #3623 (#3659)"
    
    This reverts commit e865e79f4174faa40f5fd0f8ce4a5f2416ba6f5d.
    
    * chore: add vercel e2e ci check
    
    * chore: update lint
    
    * fix: fix ci name error
    
    * chore: try to import mathjax font lib in root
    
    * fix: fix mathjax-newcm-font not found bug
    
    * fix: fix vercel lint
    
    * fix: fix vercel lint
    
    * fix: fix vercel lint
    
    * fix: add log1
    
    * fix: try
    
    * fix: fix it
    
    * fix: update
    
    * fix: update
    
    * fix: update1
    
    * fix: update2
    
    * chore: remove unused file
    
    * chore: revert markdown/index.js to #3623

---
+++ scripts/test-vercel-preview.mjs
@@ -0,0 +1,30 @@
+const baseURL = process.env.VERCEL_PREVIEW_URL;
+
+const path = `/vercel-preview-e2e-${process.env.GITHUB_SHA}`;
+const headers = {};
+
+if (process.env.VERCEL_AUTOMATION_BYPASS_SECRET) {
+  headers['x-vercel-protection-bypass'] = process.env.VERCEL_AUTOMATION_BYPASS_SECRET;
+}
+
+const listBefore = await fetch(`${baseURL}/api/comment?path=${encodeURIComponent(path)}`, {
+  headers,
+});
+
+console.log('List before:', await listBefore.json());
+
+// const post = await fetch(`${baseURL}/api/comment`, {
+//   method: 'POST',
+//   headers: {
+//     'content-type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     nick: 'Vercel Preview Tester',
+//     mail: 'vercel-preview@example.com',
+//     link: 'https://example.com',
+//     comment: 'Hello from Vercel Preview',
+//     url: path,
+//     ua: 'github-actions',
+//   }),
+// });
+// const listAfter = await fetch(`${baseURL}/api/comment?path=${encodeURIComponent(path)}`);
