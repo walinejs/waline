e84a5a6b8d7e3074596898c33c5c790532c15649
lizheming <i@imnerd.org>
2026-07-09 23:50:36 +0800

    fix: fix test

--- packages/client/__tests__/markedMathExtension.spec.ts
+++ packages/client/__tests__/markedMathExtension.spec.ts
@@ -13,53 +13,53 @@ marked.use({ extensions });
 describe('should parse inline tex', () => {
   it('single word', () => {
     expect(marked.parse('$a$')).toBe(
-      '<p><span class="wl-tex">TeX is not available in preview</span></p>\n',
+      '<p><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>a</mi></mrow><annotation encoding="application/x-tex">a</annotation></semantics></math></p>\n',
     );
 
     expect(marked.parse('$a$ is at beginning')).toBe(
-      '<p><span class="wl-tex">TeX is not available in preview</span> is at beginning</p>\n',
+      '<p><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>a</mi></mrow><annotation encoding="application/x-tex">a</annotation></semantics></math> is at beginning</p>\n',
     );
 
     expect(marked.parse('Here ends a single tex $a$')).toBe(
-      '<p>Here ends a single tex <span class="wl-tex">TeX is not available in preview</span></p>\n',
+      '<p>Here ends a single tex <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>a</mi></mrow><annotation encoding="application/x-tex">a</annotation></semantics></math></p>\n',
     );
 
     expect(marked.parse('Here is a single tex $a$ in the sentence')).toBe(
-      '<p>Here is a single tex <span class="wl-tex">TeX is not available in preview</span> in the sentence</p>\n',
+      '<p>Here is a single tex <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>a</mi></mrow><annotation encoding="application/x-tex">a</annotation></semantics></math> in the sentence</p>\n',
     );
 
     expect(marked.parse('$-$')).toBe(
-      '<p><span class="wl-tex">TeX is not available in preview</span></p>\n',
+      '<p><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo>−</mo></mrow><annotation encoding="application/x-tex">-</annotation></semantics></math></p>\n',
     );
   });
 
   it('mutiple words', () => {
     expect(marked.parse('$a = 1$')).toBe(
-      '<p><span class="wl-tex">TeX is not available in preview</span></p>\n',
+      '<p><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>a</mi><mo>=</mo><mn>1</mn></mrow><annotation encoding="application/x-tex">a = 1</annotation></semantics></math></p>\n',
     );
 
     expect(marked.parse('$a = 1$ is at beginning')).toBe(
-      '<p><span class="wl-tex">TeX is not available in preview</span> is at beginning</p>\n',
+      '<p><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>a</mi><mo>=</mo><mn>1</mn></mrow><annotation encoding="application/x-tex">a = 1</annotation></semantics></math> is at beginning</p>\n',
     );
 
     expect(marked.parse('Here ends a single tex $a = 1$')).toBe(
-      '<p>Here ends a single tex <span class="wl-tex">TeX is not available in preview</span></p>\n',
+      '<p>Here ends a single tex <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>a</mi><mo>=</mo><mn>1</mn></mrow><annotation encoding="application/x-tex">a = 1</annotation></semantics></math></p>\n',
     );
 
     expect(marked.parse('Here is a single tex $a = 1$ in the sentence')).toBe(
-      '<p>Here is a single tex <span class="wl-tex">TeX is not available in preview</span> in the sentence</p>\n',
+      '<p>Here is a single tex <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>a</mi><mo>=</mo><mn>1</mn></mrow><annotation encoding="application/x-tex">a = 1</annotation></semantics></math> in the sentence</p>\n',
     );
 
     expect(marked.parse(String.raw`$-\sqrt{x}$`)).toBe(
-      '<p><span class="wl-tex">TeX is not available in preview</span></p>\n',
+      '<p><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo>−</mo><msqrt><mi>x</mi></msqrt></mrow><annotation encoding="application/x-tex">-\\sqrt{x}</annotation></semantics></math></p>\n',
     );
 
     expect(marked.parse(String.raw`$-\sqrt{x}$ is at beginning`)).toBe(
-      '<p><span class="wl-tex">TeX is not available in preview</span> is at beginning</p>\n',
+      '<p><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo>−</mo><msqrt><mi>x</mi></msqrt></mrow><annotation encoding="application/x-tex">-\\sqrt{x}</annotation></semantics></math> is at beginning</p>\n',
     );
 
     expect(marked.parse(String.raw`Here ends a single tex $-\sqrt{x}$`)).toBe(
-      '<p>Here ends a single tex <span class="wl-tex">TeX is not available in preview</span></p>\n',
+      '<p>Here ends a single tex <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo>−</mo><msqrt><mi>x</mi></msqrt></mrow><annotation encoding="application/x-tex">-\\sqrt{x}</annotation></semantics></math></p>\n',
     );
   });
 
@@ -70,15 +70,21 @@ describe('should parse inline tex', () => {
 
 describe('should parse block tex', () => {
   it('single line', () => {
-    expect(marked.parse('$$a$$')).toBe('<p class="wl-tex">TeX is not available in preview</p>');
+    expect(marked.parse('$$a$$')).toBe(
+      '<math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><semantics><mrow><mi>a</mi></mrow><annotation encoding="application/x-tex">a</annotation></semantics></math>',
+    );
   });
 
   it('mutiple lines', () => {
-    expect(marked.parse('$$\na\n$$')).toBe('<p class="wl-tex">TeX is not available in preview</p>');
+    expect(marked.parse('$$\na\n$$')).toBe(
+      '<math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><semantics><mrow><mi>a</mi></mrow><annotation encoding="application/x-tex"> a</annotation></semantics></math>',
+    );
   });
 
   it('code block', () => {
-    expect(marked.parse('    $$a$$')).toBe('<pre><code>$$a$$\n</code></pre>\n');
+    expect(marked.parse('    $$a$$')).toBe(`<pre><code>$$a$$
+</code></pre>
+`);
 
     expect(marked.parse('```\n$$\na\n$$\n```')).toBe('<pre><code>$$\na\n$$\n</code></pre>\n');
   });
