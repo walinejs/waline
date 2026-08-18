680d5d74b45aeaee794c193f321bd02e96eff5d6
LIAO KE <104013620+iliaoke@users.noreply.github.com>
2026-08-18 23:01:04 +0800

    Update user.js

--- packages/server/src/controller/user.js
+++ packages/server/src/controller/user.js
@@ -38,7 +38,7 @@ module.exports = class UserController extends BaseRest {
 
     const formatUsers = await Promise.all(
       users.map(async (user) => {
-        await think.service('avatar').stringify({
+       user.avatar ||= await think.service('avatar').stringify({
          mail: userInfo.email,  
          nick: userInfo.display_name,
          link: userInfo.url,
